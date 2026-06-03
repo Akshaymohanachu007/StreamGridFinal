16ec0cd6f71152f2a24346f24f3f9ea3f62d4256
Akshay Mohan <akshaymohanachu007@gmail.com>
2026-06-03 18:57:55 +0530

    fix cors for vercel

--- Backend/server.js
+++ Backend/server.js
@@ -22,20 +22,19 @@ dotenv.config()
 
 const app = express();
 
-app.use(
-  cors({
-    origin: [
-      "https://stream-grid-final.vercel.app",
-      "http://localhost:5173"
-    ],
-    credentials: true,
-  })
-);
+app.use(cors({
+  origin: [
+    "https://stream-grid-final.vercel.app",
+    "https://stream-grid-final-6cnxftdal-akshaymohanachu007s-projects.vercel.app",
+    "http://localhost:5173"
+  ],
+  credentials: true,
+}));
 app.use(express.json())
 
 app.get("/", (req, res) => {
-    console.log("welcome to streamGrid Api");
-    res.send("stream grid api is running");
+  console.log("welcome to streamGrid Api");
+  res.send("stream grid api is running");
 })
 app.use("/api/auth", authRoutes)
 app.use("/api/categories", categoryRoutes);
@@ -49,31 +48,31 @@ app.use("/api/recommendations", recommendationRoutes);
 
 // Error Handling Middleware
 app.use((err, req, res, next) => {
-    const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-    res.status(statusCode).json({
-        success: false,
-        message: err.message,
-        stack: process.env.NODE_ENV === 'production' ? null : err.stack,
-    });
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  res.status(statusCode).json({
+    success: false,
+    message: err.message,
+    stack: process.env.NODE_ENV === 'production' ? null : err.stack,
+  });
 });
 
 
 // Schedule daily sync at midnight
 cron.schedule("0 0 * * *", async () => {
-    console.log("Running daily sync...");
-    await syncTrendingVideosToDB();
+  console.log("Running daily sync...");
+  await syncTrendingVideosToDB();
 });
 
 const PORT = process.env.PORT || 5000;
 
 // Start server only after DB connects
 const startServer = async () => {
-    await connectDb(); // wait for MongoDB connection before anything else
-    app.listen(PORT, () => {
-        console.log(`Server running at Port: ${PORT}`);
-    });
-    // Initial sync AFTER DB is connected
-    syncTrendingVideosToDB();
+  await connectDb(); // wait for MongoDB connection before anything else
+  app.listen(PORT, () => {
+    console.log(`Server running at Port: ${PORT}`);
+  });
+  // Initial sync AFTER DB is connected
+  syncTrendingVideosToDB();
 };
 
 startServer();
