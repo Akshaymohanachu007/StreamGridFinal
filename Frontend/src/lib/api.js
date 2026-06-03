720d263645436eca9b60d880cb23a393a5b59900
Akshay Mohan <akshaymohanachu007@gmail.com>
2026-06-03 19:15:03 +0530

    fix duplicate api url

--- Frontend/src/lib/api.js
+++ Frontend/src/lib/api.js
@@ -1,21 +1,23 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://streamgridfinal.onrender.com/api",
+
+  baseURL: `${import.meta.env.VITE_API_URL}/api`
+
 });
 
-// Automatically inject JWT token into request headers
-api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("token");
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
+
+api.interceptors.request.use((config) => {
+
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
   }
-);
+
+  return config;
+
+});
+
 
 export default api;
