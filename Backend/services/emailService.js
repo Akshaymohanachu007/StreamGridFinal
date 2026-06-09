fa417ed2f12205c06d97ca2f37f53caeed0e2ff6
Akshay Mohan <akshaymohanachu007@gmail.com>
2026-06-09 19:55:09 +0530

    fix nodemailer ipv4 issue

--- Backend/services/emailService.js
+++ Backend/services/emailService.js
@@ -2,24 +2,39 @@ import nodemailer from "nodemailer";
 
 export const sendOTP = async (email, otp) => {
 
-  
-  
   const transporter = nodemailer.createTransport({
-    service: "gmail",
+
+    host: "smtp.gmail.com",
+
+    port: 465,
+
+    secure: true,
+
+    family: 4,   // 👈 FORCE IPv4
+
     auth: {
       user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
+      pass: process.env.EMAIL_PASS
+    }
+
   });
 
+
   await transporter.sendMail({
+
     from: process.env.EMAIL_USER,
+
     to: email,
-    subject: "StreamGrid Verification Code",
-    html: `
-      <h2>Your OTP Code</h2>
-      <h1>${otp}</h1>
-      <p>This code expires in 10 minutes</p>
-    `,
+
+    subject:"StreamGrid Verification Code",
+
+    html:
+    `
+    <h2>Your OTP Code</h2>
+    <h1>${otp}</h1>
+    <p>This expires in 10 minutes</p>
+    `
+
   });
+
 };
